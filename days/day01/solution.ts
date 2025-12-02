9d1737b8d8921a73ed18f418a330b0cfd1534e6e
Michael Papesca <10820812+mpapesca@users.noreply.github.com>
2025-12-02 01:49:29 +0000

    Finish day 1 part two

--- days/day01/solution.ts
+++ days/day01/solution.ts
@@ -7,48 +7,88 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const input = fs.readFileSync(path.join(__dirname, 'input.txt'), 'utf8');
+const DIAL_SIZE = 100;
+const USE_SAMPLE = false as const;
+const TEXT_FILE = USE_SAMPLE ? 'sample.txt' : 'input.txt' as const;
+const input = fs.readFileSync(path.join(__dirname, TEXT_FILE), 'utf8');
 
 console.log("Day 1 Solution");
-type TMoveDirection = 'L' | 'R';
 
 const getNextPosition = (currentPosition: number, move: string) => {
   if (!['L', 'R'].includes(move[0])) {
     throw new Error(`Invalid move (${move}) direction: ${move[0]}`);
   }
-  const direction = move.slice(0, 1) as TMoveDirection;
+  const isClockwise = move.slice(0, 1) === 'R';
   const rotationString = move.slice(1);
   const rotation = parseInt(rotationString, 10);
   if (isNaN(rotation) || rotation < 0) {
     throw new Error(`Invalid move (${move}) value: ${rotationString}`);
   }
 
-  const positionChange = direction === 'R' ? rotation : (rotation * -1);
-  const newPosition = currentPosition + positionChange;
-  
-  // 110 % 100 = 10
-  // -20 % 100 = -20
-  const normalizedNewPosition = newPosition % 100;
-  console.log(`Processing move: ${move} -> moving ${positionChange} from ${currentPosition} to ${normalizedNewPosition}`);
-  if (normalizedNewPosition < 0) {
-    return 100 + normalizedNewPosition;
+  let crossedOriginCount = 0;
+  if (currentPosition !== 0 || rotation >= DIAL_SIZE) {  
+    const limit = currentPosition === 0 ? (
+      DIAL_SIZE
+    ) : (
+      !isClockwise ? currentPosition : DIAL_SIZE - currentPosition
+    );
+    if (rotation >= limit) {
+      const round = (rotation % DIAL_SIZE) >= limit ? Math.ceil : Math.floor;
+      crossedOriginCount += round(rotation / DIAL_SIZE);
+    }
   }
-  return normalizedNewPosition;
+
+  const positionChange = isClockwise ? rotation : (rotation * -1);
+  const newPosition = currentPosition + positionChange;
+
+  let normalizedNewPosition = newPosition % DIAL_SIZE;
+  if (normalizedNewPosition < 0) {
+    normalizedNewPosition += DIAL_SIZE;
+  }
+
+  return {
+    nextPosition: normalizedNewPosition,
+    crossedOriginCount,
+  };
 };
 
-const moves = input.split('\n');
-// Starting at 50.
-let position = 50;
-let zeroCount = 0;
-for(let move of moves) {
-  console.debug(`process: ${move}`);
-  position = getNextPosition(position, move);
-  console.log(`new position: ${position}`);
-  if (position === 0) {
-    zeroCount += 1;
+
+const partOne = () => {
+  const moves = input.split('\n');
+  // Starting at 50.
+  let position = 50;
+  let zeroCount = 0;
+  for(let move of moves) {
+    const { nextPosition } = getNextPosition(position, move);
+    if (nextPosition === 0) {
+      zeroCount += 1;
+    }
+    position = nextPosition;
   }
+
+  return zeroCount;
+};
+
+const partTwo = () => {
+  const moves = input.split('\n');
+  // Starting at 50.
+  let position = 50;
+  let clickCount = 0;
+  for(let move of moves) {
+    const { nextPosition, crossedOriginCount } = getNextPosition(position, move);
+
+    console.log(`Moved ${move} from ${position} to ${nextPosition} which did${crossedOriginCount ? '' : ' not'} click${crossedOriginCount ? ` ${crossedOriginCount} times`: ''}.`);
+    if (crossedOriginCount) {
+      console.log(`Click count change: ${clickCount} + ${crossedOriginCount} = ${clickCount + crossedOriginCount}`);
+    }
+    clickCount += crossedOriginCount;
+    position = nextPosition;
+  }
+
+  return clickCount;
 }
 
 console.log({
-  password: zeroCount,
+  partOne: partOne(),
+  partTwo: partTwo(),
 });
